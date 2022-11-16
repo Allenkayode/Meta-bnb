12c53858ccaf5c69b6827ae132464fc3a3eea088
TimiAllen <allenkayode39@gmail.com>
2022-11-16 21:10:36 +0100

    feat: fixed home design

--- src/Home.js
+++ src/Home.js
@@ -47,10 +47,10 @@ function Home() {
       <div className="body_element">
       <div  className="body-texter">
         <div className="text">
-        <h1>
+        <h1 className="head">
           Rent a <span>Place</span> away from <span>Home</span> in the <span>Metaverse</span>.
         </h1>
-        <p>
+        <p className="page">
           we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your
            imagination to reality at your comfort zone.
            </p>
@@ -59,7 +59,7 @@ function Home() {
             <Form.Control
               type="search"
               placeholder="Search"
-              className="me-2"
+              className="me-2 me"
               aria-label="Search"
             />
             <Button  className="search-btn3">Search</Button>
